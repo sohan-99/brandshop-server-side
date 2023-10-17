2ea090df231bc9b254baace606a7d64099bea0de
sohan-99 <sohan75632@gmail.com>
2023-10-18 01:09:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Server  is Runing!')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`this is port :${port}`);
+})
