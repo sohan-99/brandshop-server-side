da8912d0561a2c4aa3b0743d3eb8f627585bd68e
sohan-99 <sohan75632@gmail.com>
2023-10-21 13:50:23 +0600

    CORS Configuration

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewere
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-
+const corsConfig = {
+    origin: '*',
+    Credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}
+app.use(cors(corsConfig));
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.julqny1.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -21,99 +26,98 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-        const phoneCollation = client.db('devicedazzleDb').collection('phoneCollation');
-        const mycartPhoneCollation = client.db('mycartDB').collection('cart');
-        // get all data to database 
-        app.get('/products', async (req, res) => {
-            const result = await phoneCollation.find().toArray();
-            res.send(result);
-        })
-        // get 
-        app.get('/products/:brand_name', async (req, res) => {
-            const brand_name = req.params.brand_name
-            const query = { bandName: brand_name }
-            const result = await phoneCollation.find(query).toArray();
-            res.send(result);
-        })
-        // update single data find 
-        app.get('/update/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const user = await phoneCollation.findOne(query);
-            res.send(user)
-        })
-        // details apicheck 
-        app.get('/details/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const user = await phoneCollation.findOne(query);
-            res.send(user)
-        })
-
-        // data updata single 
-        app.put('/update/:id', async (req, res) => {
-            const id = req.params.id;
-            const updated = req.body;
-            console.log(updated);
-            const filter = { _id: new ObjectId(id) }
-            const option = { upsert: true }
-            const updetedu = {
-                $set: {
-                    image: updated.image,
-                    name: updated.name,
-                    type: updated.type,
-                    rating: updated.rating,
-                    sdec: updated.sdec,
-                    bandName: updated.bandName,
-                    price: updated.price
-
-                }
-            }
-            const result = await phoneCollation.updateOne(filter, updetedu, option)
-            res.send(result)
-        })
-        // my cart data 
-        app.get('/mycart', async (req, res) => {
-            const cursor = mycartPhoneCollation.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-        // post my cart 
-        app.post('/mycart', async (req, res) => {
-            const resCart = req.body;
-            console.log(resCart);
-            const result = await mycartPhoneCollation.insertOne(resCart);
-            res.send(result);
-        })
-        //   delete 
-        app.delete('/mycart/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await mycartPhoneCollation.deleteOne(query);
-            res.send(result);
-        })
+        // await client.connect();
 
 
-        // post 
-        app.post('/products', async (req, res) => {
-            const addp = req.body;
-            const result = await phoneCollation.insertOne(addp);
-            res.send(result);
-            console.log(phoneCollation);
-        })
-
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally { }
 }
 run().catch(console.dir);
+const phoneCollation = client.db('devicedazzleDb').collection('phoneCollation');
+const mycartPhoneCollation = client.db('mycartDB').collection('cart');
+// get all data to database 
+app.get('/products', async (req, res) => {
+    const result = await phoneCollation.find().toArray();
+    res.send(result);
+})
+// get 
+app.get('/products/:brand_name', async (req, res) => {
+    const brand_name = req.params.brand_name
+    const query = { bandName: brand_name }
+    const result = await phoneCollation.find(query).toArray();
+    res.send(result);
+})
+// update single data find 
+app.get('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const user = await phoneCollation.findOne(query);
+    res.send(user)
+})
+// details apicheck 
+app.get('/details/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const user = await phoneCollation.findOne(query);
+    res.send(user)
+})
+
+// data updata single 
+app.put('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const updated = req.body;
+    console.log(updated);
+    const filter = { _id: new ObjectId(id) }
+    const option = { upsert: true }
+    const updetedu = {
+        $set: {
+            image: updated.image,
+            name: updated.name,
+            type: updated.type,
+            rating: updated.rating,
+            sdec: updated.sdec,
+            bandName: updated.bandName,
+            price: updated.price
+
+        }
+    }
+    const result = await phoneCollation.updateOne(filter, updetedu, option)
+    res.send(result)
+})
+// my cart data 
+app.get('/mycart', async (req, res) => {
+    const cursor = mycartPhoneCollation.find();
+    const result = await cursor.toArray();
+    res.send(result);
+})
+// post my cart 
+app.post('/mycart', async (req, res) => {
+    const resCart = req.body;
+    console.log(resCart);
+    const result = await mycartPhoneCollation.insertOne(resCart);
+    res.send(result);
+})
+//   delete 
+app.delete('/mycart/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await mycartPhoneCollation.deleteOne(query);
+    res.send(result);
+})
+
+// post 
+app.post('/products', async (req, res) => {
+    const addp = req.body;
+    const result = await phoneCollation.insertOne(addp);
+    res.send(result);
+    console.log(phoneCollation);
+})
 
 
 app.get('/', (req, res) => {
     res.send('Server  is Runing!')
 })
-
 
 app.listen(port, () => {
     console.log(`this is port :${port}`);
